eeaa164279396f4b2f7a08cbd6e0b5ecffc58ae7
Shailesh Srigiri <shaileshsrigiri@gmail.com>
2024-11-07 16:45:42 -0500

    added otp authorization

---
+++ FirstApp/OTPAuthenticationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OTPAuthenticationViewController.swift
+//  FirstApp
+//
+//  Created by Shailesh Srigiri on 11/7/24.
+//
+
+import UIKit
+
+class OTPAuthenticationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
